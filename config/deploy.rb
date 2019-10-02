33b48f5b23e68882a9c6452ae9ca22a1331a1ed9
Rodrigues <fr.costa95@gmail.com>
2019-10-02 16:24:10 -0300

    continuing config cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,53 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "cap_test"
+set :repo_url, "git@github.com:RodriguesLs/cap_test.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/cap_test"
 
+append :linked_files, "config/database.yml", "config/storage.yml", "config/master.key"
+append :linked_dirs, "log", "tmp"
+ 
+set :keep_releases, 5
+set :migration_role, :app
+
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
+ 
+set :nginx_sites_available_path, "/etc/nginx/sites-available"
+set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
+ 
+# set :rvm_ruby_version, '2.4.2'
+# set :rbenv_type, :user # or :user, depends on your rbenv setup
+set :rbenv_ruby, '2.6.3'
+set :rbenv_custom_path, '/home/deploy/.rbenv'
+
+namespace :puma do
+    desc 'Create Puma dirs'
+    task :create_dirs do
+      on roles(:app) do
+        execute "mkdir #{shared_path}/tmp/sockets -p"
+        execute "mkdir #{shared_path}/tmp/pids -p"
+      end
+    end
+   
+    desc "Restart Nginx"
+    task :nginx_restart do
+      on roles(:app) do
+        execute "sudo service nginx restart"
+      end
+    end
+   
+    before :start, :create_dirs
+    after :start, :nginx_restart
+  end
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
